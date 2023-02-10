3ba1f607ae2385c913333ac052b5229baccbbe88
Vedank Naik <vedankn01@gmail.com>
2023-02-10 15:11:56 +0530

    Add/Remove cubes

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { usePlane } from "@react-three/cannon";
-import { groundTexture } from "../images/textures"
+import { groundTexture } from "../images/textures";
+import { useStore } from "../hooks/useStore";
 
 const Ground = () => {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
     position: [0, -0.5, 0],
   }));
+  const [addCube] = useStore((state) => [state.addCube]);
 
-	groundTexture.repeat.set(100, 100)
+  groundTexture.repeat.set(100, 100);
 
   return (
     <mesh
-      // onClick={(e) => {
-      //   e.stopPropagation();
-      //   const [x, y, z] = Object.values(e.point).map((val) => Math.ceil(val));
-      //   addCube(x, y, z);
-      // }}
+      onClick={(e) => {
+        e.stopPropagation();
+        const [x, y, z] = Object.values(e.point).map((val) => Math.ceil(val));
+        addCube(x, y, z);
+      }}
       ref={ref}
     >
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
